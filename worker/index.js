908fd2f797a54a6d26b999a79e05b133ca911700
myungho <skqoaudgh2@hanmail.net>
2019-12-14 17:23:01 +0900

    Github Job API를 1분마다 크롤링하는 기능 추가

---
+++ worker/index.js
@@ -0,0 +1,6 @@
+var CronJob = require('cron').CronJob;
+
+const fetchGithub = require('./tasks/fetch-github');
+
+// fetch github jobs
+new CronJob('*/1 * * * *', fetchGithub, null, true, 'America/Los_Angeles');
