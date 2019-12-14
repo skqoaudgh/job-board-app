41d15b54c2e29d3c629d0fd91ef6ac2793ee599d
myungho <skqoaudgh2@hanmail.net>
2019-12-14 16:51:04 +0900

    리액트 프로젝트 초기 설정

---
+++ client/src/Job.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography'; 
+
+export default function Job({job}) {
+    return (
+        <div className={'job'}>
+            {job.title}
+            {job.company}
+        </div>
+    )
+}
