4ff763289c964aa36dcd25461d8a9dbff671c199
myungho <skqoaudgh2@hanmail.net>
2019-12-15 15:44:56 +0900

    Junior Job 필터링 추가

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -12,6 +12,7 @@ async function fetchGithub() {
     let resultCount = 1, onPage = 0;
     const allJobs = [];
 
+    // fetch all pages
     while(resultCount > 0) {
         const res = await fetch(`${baseURL}?page=${onPage}`);
         const jobs = await res.json();
@@ -22,7 +23,23 @@ async function fetchGithub() {
     }
 
     console.log('got', allJobs.length, 'jobs total');
-    const success = await setAsync('github', JSON.stringify(allJobs));
+
+    // filter algo
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+
+        // algo logic
+        if(jobTitle.includes('senior') || jobTitle.includes('manager') || jobTitle.includes('sr.') || jobTitle.includes('architect')) {
+            return false;
+        }
+
+        return true;
+    });
+
+    console.log('filtered down to', jrJobs.length);
+
+    // set in redis
+    const success = await setAsync('github', JSON.stringify(jrJobs));
 
     console.log({success});
 }
